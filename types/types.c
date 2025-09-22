76c1d8b7123827eac85f81a62553072c2d207864
arthur andrade nunes <nunesarthur252@gmail.com>
2025-09-22 12:32:33 +0000

    hello world + types

---
+++ types/types.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int num = 0;
+    char chari[20]= "teste";
+    float decimal = 0.0;
+    double teste;
+    return 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        chari[i] = i;
+    }
+
+    printf(chari);
+
+    return 0;
+}
+
